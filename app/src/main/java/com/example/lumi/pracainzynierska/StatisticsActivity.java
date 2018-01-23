66e16decc898e13cd3cef443b0a618f7f9dbcb24
Gabriel Sochacki <gabriel-sochacki@o2.pl>
2018-01-23 19:54:00 +0100

    Improves in Stats
    Added changing kind statistics

--- app/src/main/java/com/example/lumi/pracainzynierska/StatisticsActivity.java
+++ app/src/main/java/com/example/lumi/pracainzynierska/StatisticsActivity.java
@@ -3,53 +3,111 @@ package com.example.lumi.pracainzynierska;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.Chart;
+import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.ViewPortHandler;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class StatisticsActivity extends AppCompatActivity{
+
+    int[] amounts=new int[5];
+    String[] categories = {"Rodzina","Finanse","Edukacja","Zdrowie","Kontakty"};
+    DatabaseTasks db = new DatabaseTasks(this);
+    List<PieEntry> pieEntries = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.statistics);
 
-        setupPieChart();
+        final Spinner spKind = (Spinner)findViewById(R.id.spStatKind);
+        ArrayAdapter<String> priorityAdapter = new ArrayAdapter<String>(StatisticsActivity.this,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.statsKind));
+        //set categories from <array-list> stored in strings.xml to adapter
+        priorityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spKind.setAdapter(priorityAdapter);
+        spKind.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                switch (spKind.getSelectedItem().toString())
+                {
+                    case "Liczba celów w kategorii":
+                        setupAimsOnCategoryPieChart();
+                        break;
+                    case "Liczba zadań w kategorii":
+                        setupTasksOnCategoryChart();
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                Toast.makeText(getApplicationContext(),"Nie ma nic",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
-    private void setupPieChart()
+
+    private void setupTasksOnCategoryChart()
     {
-        int[] amounts=new int[5];
-        String[] categories = {"Rodzina","Finanse","Edukacja","Zdrowie","Kontakty"};
-        DatabaseTasks db = new DatabaseTasks(this);
+        pieEntries.clear();
         for(int i = 0 ; i < categories.length;i++)
         {
-            amounts[i]=db.numberAimsInCategory(categories[i]);
-        }
-        List<PieEntry> pieEntries = new ArrayList<>();
-        for(int i=0; i < categories.length;i++)
-        {
+            amounts[i]=db.numberTasksInCategory(categories[i]);
+
             if(amounts[i]>0)
             pieEntries.add(new PieEntry(amounts[i],categories[i]));
         }
+        displayChart("Zadania");
+    }
 
-        PieDataSet pieDataSet = new PieDataSet(pieEntries,"Cele");
+    private void setupAimsOnCategoryPieChart()
+    {
+        pieEntries.clear();
+        for(int i = 0 ; i < categories.length;i++)
+        {
+            amounts[i]=db.numberAimsInCategory(categories[i]);
+            if(amounts[i]>0)
+            pieEntries.add(new PieEntry(amounts[i],categories[i]));
+        }
+        displayChart("Cele");
+    }
+
+    private void displayChart(String kind)
+    {
+        PieDataSet pieDataSet = new PieDataSet(pieEntries,kind);
         pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
         pieDataSet.setValueTextSize(18);
 
-
         PieData pieData = new PieData(pieDataSet);
+        pieData.setValueFormatter(new MyValueFormatter());
 
         Chart chart = (Chart)findViewById(R.id.chart);
         chart.setData(pieData);
         chart.animateY(1000);
         chart.invalidate();
     }
+
+
+    private class MyValueFormatter implements IValueFormatter {
+        @Override
+        public String getFormattedValue(float value, Entry entry, int dataSetIndex, ViewPortHandler viewPortHandler) {
+            
+            return new DecimalFormat("###,###,##0").format(value);
+        }
+    }
+
 }
